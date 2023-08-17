08454a3e37aec8d249cc79d586806a80c01081ab
tal0311 <tal.amit0311@gmail.com>
2023-08-17 13:21:21 +0300

    guards

--- src/cmps/RouterView.jsx
+++ src/cmps/RouterView.jsx
@@ -5,7 +5,10 @@ import {
  Outlet,
  useNavigate,
  RouterProvider,
- useMatch
+ useMatch,
+ Navigate,
+ useParams,
+ useSearchParams
 } from "react-router-dom";
 
 
@@ -21,14 +24,52 @@ import BookDetails from './../views/BookDetails.jsx'
 
 import React from 'react'
 
+// before each route
+
+const routesHistory = []
+function RouteGuard({ routeName, children }) {
+
+ const params = useParams()
+ const [searchParams] = useSearchParams();
+ const queryParams = {};
+ for (const [key, value] of searchParams) {
+  queryParams[key] = value;
+ }
+
+ const user = useSelector(state => state.userReducer.user)
+ routesHistory.push({ routeName, user, params, query })
+
+ const isLoggedIn = true
+
+
+ if (routeName === 'details') {
+  console.log('routeName', routeName);
+  // some logic
+  if (!isLoggedIn) return <Navigate to="/" />
+ }
+
+ if (routeName === 'edit') {
+  // some logic
+  if (!isLoggedIn) return <Navigate to="/" />
+ }
+ if (routeName === 'add') {
+  // some logic
+  if (!isLoggedIn) return <Navigate to="/" />
+ }
+ return <>{children}</>
+}
+
 const RouterView = () => {
  return (
   <Routes>
    <Route path="/" name="home" element={<AppHome />} />
    <Route path="/shelf" name="app" element={<AppIndex />} />
    <Route path="/explore" name="explore" element={<AppExplore />} />
-   <Route path="/shelf/:shelfId" name="details" element={<ShelfDetails />} />
-   <Route path="/shelf/:shelfId/book/:bookId" name="book-details" element={<BookDetails />} />
+
+   <Route path="/shelf/:shelfId" name="details" element={<ShelfDetails />} >
+    <Route path="/shelf/:shelfId/book/:bookId" name="book-details" element={<BookDetails />} />
+   </Route>
+
    <Route path="/login" name="login" element={<AppLogin />} />
    <Route path="/dashboard" name="dashboard" element={<DashBoard />} />
    <Route path="*" element={<Outlet />} />
