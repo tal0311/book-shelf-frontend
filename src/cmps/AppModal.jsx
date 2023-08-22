87ab03f5d0d142f6ff9a653896b407a64f178b58
tal0311 <tal.amit0311@gmail.com>
2023-08-22 19:11:28 +0300

    search modal loader

--- src/cmps/AppModal.jsx
+++ src/cmps/AppModal.jsx
@@ -4,14 +4,15 @@ import AddBook from './AddBook.jsx';
 import AddShelf from './AddShelf.jsx';
 import Explore from './../views/AppExplore.jsx';
 import { eventBus } from '../services/event-bus.service.js';
+import AppLoader from './AppLoader.jsx';
 
 const AppModal = () => {
  const [modal, setModal] = useState(null);
-
+ let unsubscribe = null
  useEffect(() => {
-  eventBus.on('openModal', setModal);
+  unsubscribe = eventBus.on('openModal', setModal);
   return () => {
-   console.log('clenaing up');
+   unsubscribe()
   };
  }, []);
 
@@ -29,7 +30,19 @@ const AppModal = () => {
   setModal(null);
  };
 
+ return (
+  <dialog ref={modalRef} className={`app-modal ${modal?.type ? modal.type : 'loading'}`}>
 
+   {modal && <DynamicCmp modal={modal} />}
+   <button className='dialog-btn icon' onClick={closeModal}>
+    <i className="material-symbols-outlined" >close</i>
+   </button>
+  </dialog>
+ );
+};
+
+
+const DynamicCmp = ({ modal }) => {
  function getModalCmp() {
   const modalTypes = {
    search: <SearchResList data={modal?.data} />,
@@ -39,15 +52,12 @@ const AppModal = () => {
   };
   return modalTypes[modal.type]
  }
-
  return (
-  <dialog ref={modalRef} className={`app-modal ${modal?.type}`}>
-   {modal && getModalCmp()}
-   <button className='dialog-btn icon' onClick={closeModal}>
-    <i className="material-symbols-outlined" >close</i>
-   </button>
-  </dialog>
- );
-};
+  <>
+   {getModalCmp()}
+  </>
+ )
+}
+
 
 export default AppModal;
--- src/cmps/SearchResList.jsx
+++ src/cmps/SearchResList.jsx
@@ -14,10 +14,11 @@ const SearchResList = ({ data }) => {
   const items = await shelfService.getItemsBySearchResults(data)
   setFilteredItems(items)
  }
-
+ // TODO: dynamic loader by Cmp
 
  return (
   <section className='search-results-list'>
+   {!filteredItems && <h1 className='modal-loader grid'>Getting your search results...</h1>}
    {filteredItems && filteredItems.map(item => {
     return (
      <article className='item-preview' key={$utils.makeId()}>
